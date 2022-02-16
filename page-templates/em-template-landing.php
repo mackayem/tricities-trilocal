4e824f813a9db10f5727823d70df7757874ca505
Emily Mackay <mackayem@pm.me>
2022-02-16 12:51:24 -0800

    adds global styles, stylizes landing page CTA for mobile

--- page-templates/em-template-landing.php
+++ page-templates/em-template-landing.php
@@ -20,9 +20,33 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 
-<div id="em-wrapperLanding" class="wrapper em-wrapper__div">
+<div id="em-wrapperLanding" class="wrapper em-wrapper__div d-flex">
+
 	<div id="em-contentLanding" class="container-fluid em-content__div">
-		<?php the_content(); ?> <!-- imports the short-code for the Email CTA by WPForms -->
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <!-- imports the all copy shown on page + short-code for Email CTA by WPForms -->
+                <?php the_content(); ?> 
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div id="em-wrapperLandingPhoto" class="em-wrapper__img">
+                    <?php 
+                        // imports the featured image on the page
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail();
+                        } else {
+                            ?>
+                            <!-- if no featured image set, use one from the assets folder -->
+                            <img id="em-landingElsePhoto" class="em-img--circle" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/imgs/placeholder_markus-spiske-mvu1-Gzg1tg-unsplash.png" alt="placeholder">
+                            <?php
+                        }
+                    ?>
+                </div>
+            </div>
+        </div><!-- row -->
 	</div><!-- #content -->
+
 </div><!-- #full-width-page-wrapper -->
 
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,11 @@
 // Add your custom JS here.
+
+// jQuery(document).ready(function($) {
+//     if( $(window).width() < 768 ) {
+//         $("#em-headingEmailCTA").html("");
+//         $("#em-headingEmailCTA").html("Subscribe for <br>Tri-Local News");
+//     } else {
+//         $("#em-headingEmailCTA").html("");
+//         $("#em-headingEmailCTA").html("Subscribe for Tri-Local News");
+//     }
+// });
