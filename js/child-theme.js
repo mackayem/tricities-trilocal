d926ddf387d7654a86407c135783e8bb52b4ee19
Emily Mackay <mackayem@pm.me>
2022-02-16 17:31:18 -0800

    corrects error in package to run gulp cmds

--- js/child-theme.js
+++ js/child-theme.js
@@ -1,6 +1,6 @@
 /*!
-  * Understrap v1.1.0 (https://understrap.com)
-  * Copyright 2013-2022 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Understrap v1.1.0 (https://www.trilocal.ca)
+  * Copyright 2013-2022 Emily Mackay (https://github.com/mackayem)
   * Licensed under GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
   */
 (function (global, factory) {
