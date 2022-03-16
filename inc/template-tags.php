793a2039292904c6a8c3a372d6f507646b67234d
Emily Mackay <35444424+mackayem@users.noreply.github.com>
2022-03-16 00:03:14 -0700

    changes template for posted on and updated on tags for single posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -17,34 +17,47 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 	function understrap_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+			$time_string_updated = '<time class="entry-date updated" datetime="%3$s">%4$s</time>';
 		}
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( 'c' ) ),
 			esc_html( get_the_date() ),
+		);
+		$time_string_updated = sprintf(
+			$time_string,
 			esc_attr( get_the_modified_date( 'c' ) ),
 			esc_html( get_the_modified_date() )
 		);
 		$posted_on   = apply_filters(
 			'understrap_posted_on',
 			sprintf(
-				'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
+				'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span><br>',
 				esc_html_x( 'Posted on', 'post date', 'understrap' ),
 				esc_url( get_permalink() ),
 				apply_filters( 'understrap_posted_on_time', $time_string )
+			)
+		);
+		$updated_on   = apply_filters(
+			'understrap_updated_on',
+			sprintf(
+				'<span class="updated-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span><br>',
+				esc_html_x( 'Updated on', 'post date', 'understrap' ),
+				esc_url( get_permalink() ),
+				apply_filters( 'understrap_updated_on_time', $time_string_updated )
 			)
 		);
 		$byline      = apply_filters(
 			'understrap_posted_by',
 			sprintf(
 				'<span class="byline"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				$posted_on ? esc_html_x( 'by', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
+				$posted_on ? esc_html_x( 'By', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
 				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 				esc_html( get_the_author() )
 			)
 		);
-		echo $posted_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $posted_on . $updated_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 
